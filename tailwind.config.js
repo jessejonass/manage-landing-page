ab6df6a210f3169ad6f616febbe1ab0d0be18e9b
Jesse Jonas <jessejonas13@gmail.com>
2022-06-21 16:24:35 -0300

    feat(init): initialized frontend mentor prject - manage landing page

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        'manage-red': {
+          700: 'hsl(12, 88%, 59%)',
+          100: 'hsl(13, 100%, 96%)',
+        },
+        'manage-slate': {
+          900: 'hsl(228, 39%, 23%)',
+        },
+        'manage-zinc': {
+          900: 'hsl(233, 12%, 13%)',
+          500: 'hsl(227, 12%, 61%)',
+          100: 'hsl(0, 0%, 98%)',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
