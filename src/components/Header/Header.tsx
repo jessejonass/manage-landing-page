ef010ec86f2374052e020d62d6abb2a8dcadc25a
Jesse Jonas <jessejonas13@gmail.com>
2022-06-30 16:44:29 -0300

    feat: updated Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
-import { FC } from 'react';
+import { Spin as Hamburger } from 'hamburger-react';
+import { FC, useState } from 'react';
 import Button from 'components/Button';
 import Link from 'components/Link';
 import Nav from './components/Nav';
 import NavItem from './components/NavItem';
 
 const Header: FC = () => {
+  const [buttonMenuIsOpen, setButtonMenuIsOpen] = useState(false);
+
   return (
     <div className='flex items-center justify-between h-16 px-4 tablet:px-16 transition-all'>
       <Link href='/'>
@@ -20,8 +23,12 @@ const Header: FC = () => {
       </Nav>
 
       <Button className='hidden tablet:block'>Get started</Button>
-      <Button transparent className='tablet:hidden'>
-        <img src='/icons/icon-hamburger.svg' alt='Logo' />
+      <Button transparent className='tablet:hidden -mr-4'>
+        <Hamburger
+          size={24}
+          toggled={buttonMenuIsOpen}
+          toggle={() => setButtonMenuIsOpen(!buttonMenuIsOpen)}
+        />
       </Button>
     </div>
   );
